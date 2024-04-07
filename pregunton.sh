d79a46c483d240f3cb14b88eaa6d0ca01359a3ba
Israel Aguilar <iaguilaror@gmail.com>
2024-04-07 00:33:52 -0600

    version 2

--- pregunton.sh
+++ pregunton.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Definimos un pais en una variable
+
+elpais="Mexico"
+
 echo "Hola, soy un script que pregunta cosas"
 
-echo "Cual es la poblacion de Mexico?"
+echo "Cual es la poblacion de $elpais?"
 
-grep "Mexico" paises.txt | cut -d"," -f2
+grep "$elpais" paises.txt | cut -d"," -f2
 
-echo "Cual es el clima de Mexico?"
+echo "Cual es el clima de $elpais?"
 
-grep "Mexico" paises.txt | cut -d"," -f3
+grep "$elpais" paises.txt | cut -d"," -f3
